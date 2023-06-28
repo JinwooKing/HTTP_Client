f7edf561bea5950577c826db40bb42db989d45d0
jinwoo park <jwpark@watechnology.co.kr>
2023-06-29 08:55:39 +0900

    비동기 요청으로 변경

--- Form1.cs
+++ Form1.cs
@@ -11,51 +11,55 @@ namespace HTTP_Client
 {
     public partial class Form1 : Form
     {
-        static RichTextBox richTextBox;
         static JsonSerializerOptions options = new JsonSerializerOptions()
         {
-            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-            WriteIndented = true,
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,          // JSON 문자열 인코딩에 사용할 인코더를 설정합니다.
+            WriteIndented = true,                                           // JSON을 들여쓰기하여 가독성을 높입니다.
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,   // null 값이 있는 속성을 직렬화할 때 무시하는 기본 동작을 설정합니다.
+            MaxDepth = 8,                                                   // 직렬화할 수 있는 개체의 최대 깊이를 설정합니다. 너무 깊은 개체는 예외를 발생시킬 수 있습니다.
+            ReadCommentHandling = JsonCommentHandling.Skip,                 // JSON 데이터에서 주석을 처리하는 방법을 설정합니다. 여기서는 주석을 건너뜁니다.
+            ReferenceHandler = ReferenceHandler.Preserve                    // 개체 참조를 유지하기 위해 참조 처리기를 설정합니다. 이렇게 하면 참조 루프가 발생할 수 있습니다.
         };
 
         public Form1()
         {
             InitializeComponent();
-            richTextBox = richTextBox2;
-
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            await HttpRequestAsync();
+            button1.Enabled = true;
+        }
+
+        private async Task HttpRequestAsync()
         {
             try
             {
-                //1. HTTP POST로 JSON  데이터 전송
-                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(textBox1.Text);
-                httpWebRequest.ContentType = "application/json";
-                string Method = string.Empty;
-                this.Invoke(new Action(delegate ()
-                {
-                    Method = comboBox1.Text;
-                }));
-                httpWebRequest.Method = Method;
+                ServicePointManager.ServerCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true;
 
-                if ("POST".Equals(Method))
+                string method = comboBox1.Text;
+                string url = textBox1.Text;
+
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.ContentType = "application/json";
+                request.Method = method;
+
+                if (method.Equals("POST", StringComparison.OrdinalIgnoreCase))
                 {
-                    var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream());
-                    streamWriter.Write(richTextBox1.Text);
-                    streamWriter.Flush();
-                    streamWriter.Close();
+                    using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                    {
+                        await streamWriter.WriteAsync(richTextBox1.Text);
+                    }
                 }
 
-                //2. Response 확인
-                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-                var streamReader = new StreamReader(httpResponse.GetResponseStream());
-                var tagInfos = JsonSerializer.Deserialize<List<TagInfo>>(streamReader.ReadToEnd());
-                //var JsonString = JsonSerializer.Serialize(tagInfos, options);
-                var JsonString = JsonSerializer.Serialize(tagInfos);
+                var response = (HttpWebResponse)await request.GetResponseAsync();
+                var streamReader = new StreamReader(response.GetResponseStream());
+                var tagInfos = JsonSerializer.Deserialize<List<TagInfo>>(await streamReader.ReadToEndAsync());
+                var jsonString = JsonSerializer.Serialize(tagInfos, options);
 
-                WriteRichTextBoxText($"{JsonString}");
+                WriteRichTextBoxText($"{jsonString}");
             }
             catch (Exception ex)
             {
@@ -65,16 +69,13 @@ namespace HTTP_Client
 
         public void WriteRichTextBoxText(string text)
         {
-            if (this.InvokeRequired)
+            if (InvokeRequired)
             {
-                this.Invoke(new Action(delegate ()
-                {
-                    richTextBox.Text = $"{text}\r\n";
-                }));
+                Invoke(new Action(() => richTextBox2.Text = $"{text}\r\n"));
             }
             else
             {
-                richTextBox.Text = $"{text}\r\n";
+                richTextBox2.Text = $"{text}\r\n";
             }
         }
 
